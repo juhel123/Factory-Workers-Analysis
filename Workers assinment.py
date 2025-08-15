a29126ea747992072eb9d318dbcd2114a4b37e3e
juhel123 <nigeriajuhel@gmail.com>
2025-08-15 15:29:40 +0100

    this project solves the analytical questions about factory workers

---
+++ Workers assinment.py
@@ -0,0 +1,512 @@
+Workers=[
+  {
+    "_id": "67d88a5b5880730695b01a3a",
+    "isActive": True,
+    "balance": "$2,952.99",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": "Lucille Collier",
+    "gender": "female",
+    "company": "MEDIOT",
+    "email": "lucillecollier@mediot.com",
+    "phone": "+1 (868) 516-3659",
+    "address": "505 Belvidere Street, Derwood, Kentucky, 8393",
+    "about": "Laborum non consectetur laboris deserunt proident officia ut ipsum. Qui esse minim commodo ullamco sit do cupidatat magna nisi voluptate dolor et consectetur. Culpa in ipsum pariatur adipisicing in sunt reprehenderit irure esse aliquip mollit.\r\n",
+    "registered": "2023-12-21T03:34:32 +08:00",
+    "tags": [
+      "nostrud",
+      "pariatur",
+      "eiusmod",
+      "pariatur",
+      "esse",
+      "nulla",
+      "adipisicing"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Roberson Griffith"
+      },
+      {
+        "id": 1,
+        "name": "Glover Pollard"
+      },
+      {
+        "id": 2,
+        "name": "Wilkinson French"
+      }
+    ],
+    "greeting": "Hello, Lucille Collier! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67d88a5bc237f9261fd64262",
+    "isActive": True,
+    "balance": "$2,471.68",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "green",
+    "name": "Hays Pope",
+    "gender": "male",
+    "company": "MITROC",
+    "email": "hayspope@mitroc.com",
+    "phone": "+1 (901) 465-3368",
+    "address": "882 Kermit Place, Gilgo, New Mexico, 555",
+    "about": "Lorem ullamco sit aliqua consectetur do. Quis commodo eu ex magna. Adipisicing commodo laboris consequat tempor consequat veniam qui dolor nisi occaecat. Ad velit commodo irure voluptate ullamco. Ut aliquip est excepteur tempor cupidatat eu. Sunt qui anim sint laborum non. Exercitation qui ut excepteur ad velit officia laborum deserunt aliqua cillum minim consequat enim.\r\n",
+    "registered": "2021-11-27T03:30:39 +08:00",
+    "tags": [
+      "dolor",
+      "voluptate",
+      "pariatur",
+      "laboris",
+      "nulla",
+      "dolore",
+      "excepteur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Sheri Bauer"
+      },
+      {
+        "id": 1,
+        "name": "Charmaine Moon"
+      },
+      {
+        "id": 2,
+        "name": "John Joyner"
+      }
+    ],
+    "greeting": "Hello, Hays Pope! You have 2 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "67d88a5b0059b84ec037e64f",
+    "isActive": True,
+    "balance": "$3,073.55",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "brown",
+    "name": "Burris Hobbs",
+    "gender": "male",
+    "company": "ISONUS",
+    "email": "burrishobbs@isonus.com",
+    "phone": "+1 (819) 461-2013",
+    "address": "377 Bragg Court, Belmont, American Samoa, 3901",
+    "about": "Laborum eiusmod eu qui et minim dolor in. Consequat exercitation cupidatat elit exercitation exercitation id occaecat labore sunt Lorem adipisicing cupidatat labore. Incididunt veniam nisi elit occaecat.\r\n",
+    "registered": "2018-05-09T08:36:48 +07:00",
+    "tags": [
+      "nisi",
+      "anim",
+      "velit",
+      "mollit",
+      "qui",
+      "fugiat",
+      "dolore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Rebecca Mays"
+      },
+      {
+        "id": 1,
+        "name": "Hinton Horne"
+      },
+      {
+        "id": 2,
+        "name": "Woods Gamble"
+      }
+    ],
+    "greeting": "Hello, Burris Hobbs! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67d88a5b62042198f9ca1e14",
+    "isActive": True,
+    "balance": "$1,001.01",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": "Rosella Kirkland",
+    "gender": "female",
+    "company": "VORATAK",
+    "email": "rosellakirkland@voratak.com",
+    "phone": "+1 (872) 458-2142",
+    "address": "765 Chester Court, Goodville, Ohio, 4835",
+    "about": "Occaecat excepteur non aliquip amet. Est consequat velit ex labore deserunt ipsum elit id officia quis aute voluptate. Anim magna occaecat fugiat laboris.\r\n",
+    "registered": "2019-03-01T09:13:42 +08:00",
+    "tags": [
+      "officia",
+      "veniam",
+      "tempor",
+      "officia",
+      "nulla",
+      "laboris",
+      "ullamco"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Noel Durham"
+      },
+      {
+        "id": 1,
+        "name": "Angie Ware"
+      },
+      {
+        "id": 2,
+        "name": "Stella Hampton"
+      }
+    ],
+    "greeting": "Hello, Rosella Kirkland! You have 3 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "67d88a5be72445ed5189f5f6",
+    "isActive": True,
+    "balance": "$1,565.81",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "green",
+    "name": "Sherri Terry",
+    "gender": "female",
+    "company": "ENTROFLEX",
+    "email": "sherriterry@entroflex.com",
+    "phone": "+1 (869) 490-2826",
+    "address": "673 Chapel Street, Wescosville, Montana, 1670",
+    "about": "Nisi deserunt consectetur aute commodo aute. Esse et officia sit non deserunt minim consectetur aute sit elit sit. Dolor sunt in excepteur occaecat id commodo Lorem. Fugiat ut ea adipisicing anim aliqua occaecat tempor sunt do aute tempor nostrud. Do ea do deserunt proident ex id magna ut veniam et sint.\r\n",
+    "registered": "2025-02-11T04:25:41 +08:00",
+    "tags": [
+      "tempor",
+      "non",
+      "id",
+      "sunt",
+      "consectetur",
+      "laborum",
+      "exercitation"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Juanita Glass"
+      },
+      {
+        "id": 1,
+        "name": "Williams Frank"
+      },
+      {
+        "id": 2,
+        "name": "Eddie Colon"
+      }
+    ],
+    "greeting": "Hello, Sherri Terry! You have 1 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "67d88a5bd4df4132df609ad2",
+    "isActive": False,
+    "balance": "$3,792.42",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "brown",
+    "name": "Mollie Hayes",
+    "gender": "female",
+    "company": "FARMAGE",
+    "email": "molliehayes@farmage.com",
+    "phone": "+1 (973) 521-3538",
+    "address": "605 Bedford Place, Websterville, District Of Columbia, 103",
+    "about": "Aute deserunt reprehenderit eiusmod incididunt reprehenderit culpa dolore occaecat esse est veniam commodo irure. Duis ad exercitation aliqua incididunt. Ad sint anim ullamco minim adipisicing velit labore. Commodo deserunt consectetur ut est reprehenderit. In minim esse pariatur proident anim pariatur reprehenderit cillum nulla pariatur aliqua.\r\n",
+    "registered": "2016-11-28T12:32:51 +08:00",
+    "tags": [
+      "pariatur",
+      "non",
+      "reprehenderit",
+      "anim",
+      "in",
+      "nostrud",
+      "sint"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Faulkner Wise"
+      },
+      {
+        "id": 1,
+        "name": "Holman Phelps"
+      },
+      {
+        "id": 2,
+        "name": "Hess Blankenship"
+      }
+    ],
+    "greeting": "Hello, Mollie Hayes! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "67d88a5ba2556694c6e551f1",
+    "isActive": False,
+    "balance": "$1,408.91",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "brown",
+    "name": "Doris George",
+    "gender": "female",
+    "company": "LEXICONDO",
+    "email": "dorisgeorge@lexicondo.com",
+    "phone": "+1 (953) 560-2430",
+    "address": "287 Canda Avenue, Boomer, Palau, 9103",
+    "about": "Culpa voluptate incididunt do anim ea aute aute in sunt sunt officia veniam reprehenderit. Eiusmod eiusmod cupidatat cupidatat consectetur exercitation et velit. Ex occaecat est nisi ut sit aliqua cillum minim laboris aliquip in incididunt duis. Culpa ea ipsum quis do labore aliqua duis exercitation fugiat proident et sunt aute. Fugiat reprehenderit laborum voluptate duis anim magna duis tempor. Minim nisi magna nostrud do cupidatat qui irure laboris ut. Ullamco fugiat irure est duis ipsum quis sit nisi ea.\r\n",
+    "registered": "2022-08-29T04:43:18 +07:00",
+    "tags": [
+      "labore",
+      "dolore",
+      "ad",
+      "occaecat",
+      "est",
+      "officia",
+      "ea"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Bonita Sargent"
+      },
+      {
+        "id": 1,
+        "name": "Eloise Copeland"
+      },
+      {
+        "id": 2,
+        "name": "Jenny Vasquez"
+      }
+    ],
+    "greeting": "Hello, Doris George! You have 6 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67d88a5b6980a2fa27decb43",
+    "isActive": True,
+    "balance": "$3,235.71",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": "Rasmussen Mosley",
+    "gender": "male",
+    "company": "MENBRAIN",
+    "email": "rasmussenmosley@menbrain.com",
+    "phone": "+1 (941) 477-3810",
+    "address": "755 Narrows Avenue, Jessie, New Hampshire, 2573",
+    "about": "Non sunt sunt excepteur sint consequat ex Lorem quis in sint irure eu tempor. Aliqua pariatur elit proident sunt. Enim Lorem nisi est et id mollit enim velit irure. Non officia amet pariatur et ex laboris. Excepteur esse nulla amet excepteur ut cupidatat fugiat do tempor pariatur nulla adipisicing. Est exercitation amet et sit ad qui consequat consectetur eiusmod. Incididunt ut occaecat eu ullamco dolore laboris ex quis velit Lorem cillum laboris.\r\n",
+    "registered": "2015-07-11T07:28:26 +07:00",
+    "tags": [
+      "sunt",
+      "eu",
+      "dolore",
+      "consectetur",
+      "enim",
+      "ea",
+      "tempor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Patricia Nash"
+      },
+      {
+        "id": 1,
+        "name": "Ashley Riggs"
+      },
+      {
+        "id": 2,
+        "name": "Thompson Huff"
+      }
+    ],
+    "greeting": "Hello, Rasmussen Mosley! You have 1 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "67d88a5b26aac8c2ae40cfc8",
+    "isActive": False,
+    "balance": "$2,689.70",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "brown",
+    "name": "Chan Potter",
+    "gender": "male",
+    "company": "DIGIRANG",
+    "email": "chanpotter@digirang.com",
+    "phone": "+1 (829) 474-3957",
+    "address": "208 Ferris Street, Sugartown, Nevada, 8108",
+    "about": "In voluptate ut est cupidatat cillum reprehenderit aliqua commodo sint Lorem nostrud voluptate veniam occaecat. Sit exercitation id eiusmod culpa. Laboris do eu duis Lorem labore dolor officia enim. Est consequat laboris culpa nulla labore commodo officia enim mollit aute nisi aliquip. Et officia adipisicing quis veniam Lorem aliqua reprehenderit irure. Cillum in quis ad exercitation mollit Lorem laboris tempor occaecat aute velit officia.\r\n",
+    "registered": "2015-12-01T12:18:46 +08:00",
+    "tags": [
+      "veniam",
+      "nisi",
+      "ut",
+      "aliqua",
+      "nulla",
+      "dolore",
+      "excepteur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Adkins Keith"
+      },
+      {
+        "id": 1,
+        "name": "Boyd Chandler"
+      },
+      {
+        "id": 2,
+        "name": "Lara Horton"
+      }
+    ],
+    "greeting": "Hello, Chan Potter! You have 8 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67d88a5bfd7baa3b52df5666",
+    "isActive": True,
+    "balance": "$1,252.96",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "green",
+    "name": "Roth Whitfield",
+    "gender": "male",
+    "company": "PYRAMIA",
+    "email": "rothwhitfield@pyramia.com",
+    "phone": "+1 (878) 511-3017",
+    "address": "472 Oceanic Avenue, Chesterfield, Hawaii, 8056",
+    "about": "Et fugiat exercitation amet eu tempor sunt aute exercitation cupidatat sit enim magna eiusmod. Qui et do excepteur dolore Lorem nostrud labore ad enim esse ullamco est. Et ad dolore occaecat consequat eu. Veniam voluptate Lorem ex velit sint ex amet amet.\r\n",
+    "registered": "2023-03-01T03:57:42 +08:00",
+    "tags": [
+      "mollit",
+      "eu",
+      "tempor",
+      "reprehenderit",
+      "sit",
+      "est",
+      "dolore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Janell Faulkner"
+      },
+      {
+        "id": 1,
+        "name": "Mari Berry"
+      },
+      {
+        "id": 2,
+        "name": "Baldwin Gross"
+      }
+    ],
+    "greeting": "Hello, Roth Whitfield! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+  }
+]
+
+# fruits = ['apple', 'banana', 'cherry', 'apple', 'cherry', 'cherry']
+# x = fruits.count('apple')
+# print(x)
+
+#ASSIGNMENT ON PYTHON DICTIONARY
+
+#Number of workers whose age is above 30
+
+Workers_count = 0
+for x in Workers:
+      if x['age'] > 30:
+            Workers_count = Workers_count + 1
+print(f'number of workers whose age are above 30 is', Workers_count)
+      
+y = 0
+for x in Workers:
+      if x['age'] > 30:
+            y = y + 1
+            
+print(f'number of workers whose age are above 30 is', y)
+
+#The worker with the highest balance
+
+highestbalance = 0
+for w in Workers:
+      balance = float(w['balance'].replace('$','').replace(',',''))
+      if balance > highestbalance:
+            highestbalance = balance
+            name = w['name']
+print(f'the worker the highest balance is {name} and {highestbalance}')
+
+
+#HOW MANY WORKERS ARE ACTIVE AND INACTIVE?
+y = 0
+b = 0
+for x in Workers:
+      if x['isActive'] == True:
+            y = y + 1
+      else:
+            ['isActive'] == False
+            b = b + 1
+print(f'number of active workers are {y} and inactive are {b}')
+  
+#HOW MANY WORKERS ARE MALE AND FEMALE?
+#BELOW IS HOW TO WRITE THE CODE TOGETHER
+c = 0
+d = 0
+for x in Workers:
+      if x['gender'] == 'male' and 'female': 
+            c = c + 1
+            d = d + 1
+print(f'male workers are {c} while female workers are {d}')
+
+
+# BELOW IS HOW TO WRITE THE CODE SEPERATELY
+e = 0
+for x in Workers:
+      if x['gender'] == 'female':
+            e = e + 1
+print('female workers are',e)
+
+f = 0
+for x in Workers:
+      if x['gender'] == 'male':
+            f = f + 1
+print('male workers are',f)
+
+#WHAT ARE THE EYE COLORS THAT WE HAVE IN OUR DATA?
+eyeColor = []
+for z in Workers:
+        if z['eyeColor'] not in eyeColor:
+         eyeColor.append(z['eyeColor'])
+print(eyeColor)
+ 
+
+
+
+
+  
+ 
+   
+
+
+
+
+
+    
+
+
+
+
+
+    
+    
+
+
+
+
+    
